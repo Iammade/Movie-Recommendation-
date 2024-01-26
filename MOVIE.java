050b6b8cfe701b66ba3de8bfcf2f8389ce0c1a23
Ravi Teja B N <100072245+Iammade@users.noreply.github.com>
2024-01-26 22:54:57 +0530

    Add files via upload

---
+++ MOVIE.java
@@ -0,0 +1,425 @@
+package movie;
+
+import java.util.*;
+import java.lang.System;
+
+public class movie
+{
+	static void Cartoon()
+	{
+		ArrayList<String> cartoon = new ArrayList<>();
+		cartoon.add(" 1->Chota Bheem\r");
+		cartoon.add("2->Ben 10\r");
+		cartoon.add("3->Power and Rangers\r");
+		cartoon.add("4->krish\r");
+		cartoon.add("5->Mighty Raju\r");
+		cartoon.add("6->Oggy and the Cockroaches\r");
+		cartoon.add("7->Motu Patlu\r");
+		cartoon.add("8->Dora, The Explorer\r");
+		cartoon.add("9->Doraemon\r");
+		cartoon.add("10->Shinchan\r");
+		System.out.println(cartoon);
+	}
+	
+	void Comedy() 
+	{
+		ArrayList<String> tl = new ArrayList<>();
+		tl.add(" 1->Mr Bean\r");
+		tl.add("2->Charlie Chaplin\r");
+		tl.add("3->Tom & Jerry\r");
+		tl.add("3->ninja hattor\r");
+		tl.add("4->Oggy and the Cockroaches \r");
+		tl.add("5->Roll No. 21\r");
+		tl.add("6->Teen Titans Go\r");
+		tl.add("7->pakdam pakdai\r");
+		tl.add("8->shiv\r");
+		tl.add("9->SpongeBob SquarePants\r");
+		tl.add("10->danger school\r");
+		System.out.println(tl);
+	}
+	void k_Horror()
+	{
+		ArrayList<String> horror = new ArrayList<>();
+		
+		horror.add("1. Aake (2017) - IMDb rating 7.0 -  Amazon Prime Video\r");
+		horror.add("2. Karva (2016) - IMDb rating 6.8 -  Amazon Prime Video\r");
+		horror.add("3. Jigarthanda (2016) - IMDb rating 6.5 -  Amazon Prime Video\r");
+		horror.add("4. 6-5=2 (2013) - IMDb rating 6.4 -  Amazon Prime Video\r");
+		horror.add("5. Namo Bhootatma (2014) - IMDb rating 5.7 -  Amazon Prime Video\r");
+		horror.add("6. Charminar (2013) - IMDb rating 5.6 -  Amazon Prime Video\r");
+		horror.add("7. Mummy - Save Me (2016) - IMDb rating 5.3 -  Amazon Prime Video\r");
+		horror.add("8. Last Bus (2016) - IMDb rating 5.1 -  Amazon Prime Video\r");
+		horror.add("9. Chandralekha (2014 -​ IMDb rating 4.8 - Amazon Prime Video\r");
+		System.out.println(horror);
+	}
+	void k_Thriller() 
+	{
+		ArrayList<String> thriller= new ArrayList<>();
+		thriller.add("1. KGF: Chapter 1 (2018) - IMDb rating 8.2 - Amazon Prime Video\r\n"
+				+ "2. U Turn (2016) - IMDb rating 7.7 -  Netflix\r\n"
+				+ "3. Lucia (2013) - IMDb rating 8.3 -  Amazon Prime Video\r\n"
+				+ "4. RangiTaranga (2015) - IMDb rating 8.3 -  Amazon Prime Video\r\n"
+				+ "5. Kendasampige (2015) - IMDb rating 8.1 -  Amazon Prime Video\r\n"
+				+ "6. Ugramm (2014) - IMDb rating 8.1 -  Amazon Prime Video\r\n"
+				+ "7. Shuddhi (2017) - IMDb rating 7.9 -  Amazon Prime Video\r\n"
+				+ "8. Kavaludaari (2019) - IMDb rating 8.4 -  Amazon Prime Video\r\n"
+				+ "9. Tagaru (2018) - IMDb rating 7.8 -  Amazon Prime Video\r\n"
+				+ "10. Dandupalya 2 (2017) - IMDb rating 7.3 - Amazon Prime Video");
+		System.out.println(thriller);
+	}	
+	void k_drama()
+	{
+		ArrayList<String> drama= new ArrayList<>();
+		drama.add("1. Thithi (2015) - IMDb rating 8.0 -  Amazon Prime Video\r\n"
+				+ "2. Godhi Banna Sadharana Mykattu (2016) - IMDb rating 8.3 -  Amazon Prime Video\r\n"
+				+ "3. Mungaru Male (2006) - IMDb rating 8.2 -  Amazon Prime Video\r\n"
+				+ "4. Sairat (2016) - IMDb rating 8.2 -  Netflix\r\n"
+				+ "5. Lucia (2013) - IMDb rating 8.3 -  Amazon Prime Video\r\n"
+				+ "6. KGF: Chapter 1 (2018) - IMDb rating 8.2 -  Amazon Prime Video\r\n"
+				+ "7. RangiTaranga (2015) - IMDb rating 8.3 -  Amazon Prime Video\r\n"
+				+ "8. Kirik Party (2016) - IMDb rating 8.4 -  Amazon Prime Video\r\n"
+				+ "9. U Turn (2016) - IMDb rating 7.7 -  Netflix\r\n"
+				+ "10. Rama Rama Re (2016) - IMDb rating 8.0 -  Amazon Prime Video");
+		System.out.println(drama);
+	}
+	void k_action() 
+	{
+		ArrayList<String> act = new ArrayList<>();
+		act.add("1. KGF: Chapter 1 (2018) - IMDb rating 8.2 -  Amazon Prime Video\r\n"
+				+ "2. Raajakumara (2017) - IMDb rating 8.2 -  Amazon Prime Video\r\n"
+				+ "3. Mufti (2017) - IMDb rating 8.2 -  Amazon Prime Video\r\n"
+				+ "4. Hebbuli (2017) - IMDb rating 7.4 -  Amazon Prime Video\r\n"
+				+ "5. Doddmane Hudga (2016) - IMDb rating 7.3 -  Amazon Prime Video\r\n"
+				+ "6. Gajakesari (2014) - IMDb rating 7.2 -  Amazon Prime Video\r\n"
+				+ "7. Chakravyuha (2016) - IMDb rating 7.1 -  Amazon Prime Video\r\n"
+				+ "8. Ranna (2015) - IMDb rating 7.1 -  Amazon Prime Video\r\n"
+				+ "9. KGF: Chapter 2 (2021) - IMDb rating 7.1 - Not  any platform currently\r\n"
+				+ "10. Victory 2 (2018) - IMDb rating 6.8 -  Amazon Prime Video");
+		System.out.println(act);
+	}
+	void k_comedy() 
+	{
+		ArrayList<String>com = new ArrayList<>();
+		com.add("1. Lucia (2013) - IMDb rating 8.3 -  Amazon Prime Video\r\n"
+				+ "2. Mungaru Male (2006) - IMDb rating 8.2 -  Amazon Prime Video\r\n"
+				+ "3. Simple Agi Ondh Love Story (2013) - IMDb rating 8.0 -  Amazon Prime Video\r\n"
+				+ "4. Kendasampige (2015) - IMDb rating 8.1 -  Amazon Prime Video\r\n"
+				+ "5. Rangitaranga (2015) - IMDb rating 8.3 -  Amazon Prime Video\r\n"
+				+ "6. Sarkari Hiriya Prathamika Shale Kasaragodu Koduge: Ramanna Rai, (2018) - IMDb rating 8.2 -  Amazon Prime Video\r\n"
+				+ "7. Kirik Party (2016) IMDb - rating 8.4 -  Amazon Prime Video\r\n"
+				+ "8. Zoom (2016) - IMDb rating 7.9 -  Amazon Prime Video\r\n"
+				+ "9. Drama (2012) - IMDb rating 7.6 -  Amazon Prime Video\r\n"
+				+ "10. Churikatte (2018) - IMDb rating 7.4 -  Amazon Prime Video");
+		System.out.println(com);
+	}
+	void e_Horror()
+	{
+		ArrayList<String> e_horror = new ArrayList<>();
+		e_horror.add("English horror movies \r\n"
+				+ "1. A Quiet Place (2018) - IMDb rating 7.5 - Amazon Prime Video\r\n"
+				+ "2. Hereditary (2018) - IMDb rating 7.3 -  Amazon Prime Video\r\n"
+				+ "3. The Conjuring (2013) - IMDb rating 7.5 -  Netflix\r\n"
+				+ "4. The Babadook (2014) - IMDb rating 6.8 -  Netflix\r\n"
+				+ "5. It (2017) - IMDb rating 7.3 -  Amazon Prime Video\r\n"
+				+ "6. Insidious (2010) - IMDb rating 6.8 -  Netflix\r\n"
+				+ "7. The Witch (2015) - IMDb rating 6.9 -  Netflix\r\n"
+				+ "8. Get Out (2017) - IMDb rating 7.7 -  Amazon Prime Video\r\n"
+				+ "9. Annabelle: Creation (2017) - IMDb rating 6.5 -  Netflix\r\n"
+				+ "10. The Exorcist (1973) - IMDb rating 8.0 -  Amazon Prime Video");
+		System.out.println(e_horror);
+	}
+	void h_Horror() 
+	{
+		ArrayList<String> h_horror = new ArrayList<>();
+		h_horror.add("1. Tumbbad (2018) - IMDb rating 8.2 -  Amazon Prime Video\r\n"
+				+ "2. Stree (2018) - IMDb rating 7.6 -  Netflix\r\n"
+				+ "3. Bhoot: Part One - The Haunted Ship (2020) - IMDb rating 6.1 -  Amazon Prime Video\r\n"
+				+ "4. Pari (2018) - IMDb rating 6.6 -  Netflix\r\n"
+				+ "5. Raat (1992) - IMDb rating 7.0 -  Netflix\r\n"
+				+ "6. 1920 (200) - IMDb rating 6.4 -  Netflix\r\n"
+				+ "7. Darna Mana Hai (2003) - IMDb rating 6.3 -  Netflix\r\n"
+				+ "8. Horror Story (2013) - IMDb rating 5.3 -  Netflix\r\n"
+				+ "9. Ragini MMS (2011) - IMDb rating 4.9 -  Netflix\r\n"
+				+ "10. 13B (2009) - IMDb rating 7.0 -  Netflix");
+		System.out.println(h_horror);
+	}
+	void e_Thriller()
+	{
+		ArrayList<String> e_thriller= new ArrayList<>();
+		e_thriller.add("1. The Silence of the Lambs (8.6) - Netflix, Amazon Prime Video, and Hulu\r\n"
+				+ "2. The Departed (8.5) -  Netflix\r\n"
+				+ "3. Se7en (8.6) -  Netflix\r\n"
+				+ "4. Inception (8.8) -  Netflix\r\n"
+				+ "5. The Prestige (8.5) - Netflix\r\n"
+				+ "6. The Girl with the Dragon Tattoo (7.8) -  Netflix\r\n"
+				+ "7. The Bourne Ultimatum (8.0) -  Netflix\r\n"
+				+ "8. Gone Girl (81) -  Amazon Prime Video\r\n"
+				+ "9. The Handidenma (8.1) -  Amazon Prime Video\r\n"
+				+ "10. The Invisible Guest (8.1) -  Netflix");
+		System.out.println(e_thriller);
+	}
+	void h_Thriller()
+	{
+		ArrayList<String> h_thriller= new ArrayList<>();
+		h_thriller.add("1. Andhadhun (2018) - IMDb rating 8.3 -  Netflix\r\n"
+				+ "2. Kahaani (2012) - IMDb rating 8.1 -  Netflix\r\n"
+				+ "3. Drishyam (2015) - IMDb rating 8.2 -  Amazon Prime Video\r\n"
+				+ "4. Special 26 (2013) - IMDb rating 8.0 -  Amazon Prime Video\r\n"
+				+ "5. Ugly (2013) - IMDb rating 8.1 -  Netflix\r\n"
+				+ "6. Talaash (2012) IMDb - rating 7.2 -  Netflix\r\n"
+				+ "7. NH10 (2015) - IMDb rating 7.2 -  Netflix\r\n"
+				+ "8. Ittefaq (2017) - IMDb rating 7.2 -  Netflix\r\n"
+				+ "9. Badla (2019) - IMDb rating 7.9 -  Netflix\r\n"
+				+ "10. Ek Hasina Thi (2004) - IMDb rating 7.5 -  Netflix");
+		System.out.println(h_thriller);
+		
+	}
+	void e_drama()
+	{
+		ArrayList<String> e_drama= new ArrayList<>();
+		e_drama.add("\"1. Gangs of Wasseypur (2012) - IMDb rating 8.2 -  Netflix\\r\\n\"\r\n"
+				+ "2. Baahubali: The Beginning (2015) - IMDb rating 8.1 -  Netflix\\r\\n\"\r\n"
+				+ "3. Bajrangi Bhaijaan (2015) - IMDb rating 8.0 -  Netflix\\r\\n\"\r\n"
+				+ "4. Drishyam (2015) - IMDb rating 8.2 -  Amazon Prime Video\\r\\n\"\r\n"
+				+ "5. Baby (2015) - IMDb rating 8.0 -  Netflix\\r\\n\"\r\n"
+				+ "6. Special 26 (2013) - IMDb rating 8.0 -  Amazon Video Prime\\r\\n\"\r\n"
+				+"7. Dangal (2016) - IMDb rating 8.4 -  Netflix\\r\\n\"\r\n"
+				+ "8. Uri: The Surgical Strike (2019) - IMDb rating 8.3 -  ZEE5\\r\\n\"\r\n"
+				+"9. Kabir Singh (2019) - IMDb rating 7.1 -  Netflix\\r\\n\"		\r\n"
+				+ "10. Raees (2017) - IMDb rating 6.8 -  Netflix");
+		System.out.println(e_drama);
+	}
+	void h_drama() 
+	{
+		ArrayList<String> h_drama= new ArrayList<>();
+		h_drama.add("\r\n"
+				+ "1. Taare Zameen Par (2007) - IMDb rating 8.4 -  Netflix\r\n"
+				+ "2. Dil Chahta Hai (2001) - IMDb rating 8.1 -  Amazon Prime Video\r\n"
+				+ "3. Lagaan (2001) - IMDb rating 8.1 -  Netflix\r\n"
+				+ "4. Rang De Basanti (2006) - IMDb rating 8.2 -  Netflix\r\n"
+				+ "5. Black (2005) - IMDb rating 8.2 -  Netflix\r\n"
+				+ "6. Swades (2004) - IMDb rating 8.2 -  Netflix\r\n"
+				+ "7. Anand (1971) - IMDb rating 8.5 -  Amazon Prime Video\r\n"
+				+ "8. A Wednesday (2008) - IMDb rating 8.1 -  Netflix\r\n"
+				+ "9. Pink (2016) - IMDb rating 8.1 -  Netflix\r\n"
+				+ "10. Udta Punjab (2016) - IMDb rating 7.8 - Netflix");
+		System.out.println(h_drama);
+	}
+	void e_action()
+	{
+		ArrayList<String> e_act = new ArrayList<>();
+		e_act.add("1. The Dark Knight (2008) (9.0) -  Netflix\r\n"
+				+ "2. Terminator 2: Judgment Day (1991) (8.5) -  Netflix\r\n"
+				+ "3. Die Hard (1988) (8.2) -  Netflix\r\n"
+				+ "4. The Matrix (1999) (8.7) -  Netflix\r\n"
+				+ "5. Mad Max: Fury Road (2015) (8.1) -  Netflix\r\n"
+				+ "6. John Wick (2014) (7.4) -  Netflix\r\n"
+				+ "7. Mission: Impossible - Fallout (2018) (7.7) -  Amazon Prime Video\r\n"
+				+ "8. Casino Royale (2006) (8.0) -  Netflix\r\n"
+				+ "9. Kill Bill: Vol. 1 (2003) (8.1) -  Netflix\r\n"
+				+ "10. Fast Five (2011) (7.3) -  Netflix");
+		System.out.println(e_act);
+	}
+	void h_action() 
+	{
+		ArrayList<String> h_act = new ArrayList<>();
+		h_act.add("1. Gangs of Wasseypur (2012) - IMDb rating 8.2 -  Netflix\r\n"
+				+ "2. Baahubali: The Beginning (2015) - IMDb rating 8.1 -  Netflix\r\n"
+				+ "3. Bajrangi Bhaijaan (2015) - IMDb rating 8.0 -  Netflix\r\n"
+				+ "4. Drishyam (2015) - IMDb rating 8.2 -  Amazon Prime Video\r\n"
+				+ "5. Baby (2015) - IMDb rating 8.0 -  Netflix\r\n"
+				+ "6. Special 26 (2013) - IMDb rating 8.0 -  Amazon Video Prime\r\n"
+				+ "7. Dangal (2016) - IMDb rating 8.4 -  Netflix\r\n"
+				+ "8. Uri: The Surgical Strike (2019) - IMDb rating 8.3 -  ZEE5\r\n"
+				+ "9. Kabir Singh (2019) - IMDb rating 7.1 -  Netflix\r\n"		
+				+ "10. Raees (2017) - IMDb rating 6.8 -  Netflix");
+		System.out.println(h_act);
+	}
+	void e_comedy()
+	{
+		ArrayList<String> e_com = new ArrayList<>();
+		e_com.add("1. The Grand Budapest Hotel (2014) (8.1) -  Netflix\r\n"
+				+ "2. The Hangover (2009) (7.7) -  Netflix\r\n"
+				+ "3. Superbad (2007) (7.6) -  Netflix\r\n"
+				+ "4. Bridesmaids (2011) (6.8) -  Netflix\r\n"
+				+ "5. Ghostbusters (1984) (7.8) -  Netflix\r\n"
+				+ "6. This Is Spinal Tap (1984) (8.0) -  Amazon Prime Video\r\n"
+				+ "7. Monty Python and Holy the Grail (1975) (8.2) -  Netflix\r\n"
+				+ "8. The 40-Year-Old Virgin (2005) (7.1) -  Netflix\r\n"
+				+ "9. Groundhog Day (1993) (8.0) -  Netflix\r\n"
+				+ "10. Little Miss Sunshine (2006) (7.8) -  Netflix");
+		System.out.println(e_com);
+	}
+	void h_comedy()
+	{
+		ArrayList<String> h_com = new ArrayList<>();
+		h_com.add("1. Andaz Apna Apna (1994) - IMDb rating 8.2 -  Netflix\r\n"
+				+ "2. Hera Pheri (2000) - IMDb rating 8.2 -  Netflix\r\n"
+				+ "3. Munna Bhai M.B.B.S. (2003) - IMDb rating 8.1 -  Netflix\r\n"
+				+ "4. 3 Idiots (2009) - IMDb rating 8.4 -  Netflix\r\n"
+				+ "5. Golmaal: Fun Unlimited (2006) - IMDb rating 7.4 -  Amazon Prime Video\r\n"
+				+ "6. Dhamaal (2007) - IMDb rating 7.3 -  Amazon Prime Video\r\n"
+				+ "7. Chupke Chupke (1975) - IMDb rating 8.4 -  Netflix\r\n"
+				+ "8. Piku201 (5) - IMDb rating 7.6 -  Amazon Prime Video\r\n"
+				+ "9. Munna Bhai M.B.B.S. (2003) - IMDb rating 8.1 -  Netflix\r\n"
+				+ "10. Dil Chahta Hai (2001) - IMDb rating 8.1 -  Amazon Prime Video");
+		System.out.println(h_com);
+	}
+	public static void main(String[] args)
+	{
+		Scanner a = new Scanner(System.in);
+		System.out.println("*WELCOME TO MOVIE RECOMMENDATION SYSTEM*");
+		System.out.println("We are going to recommend you some amazing  movies and shows(few in English) based on your choice:");
+		System.out.println("Enter the age : ");
+		int age = a.nextInt();
+		if(age <= 0)
+		{
+			System.out.println("Invalid Age");
+			System.exit(0);
+		}
+		if( age > 140)
+		{
+			System.out.println("Invalid Age");
+			System.exit(0);
+		}
+		String name = a.nextLine();
+		movie Com =  new movie();
+		movie k_hor = new movie();
+		movie k_tir = new movie();
+		movie k_dra = new movie();
+		movie k_act = new movie();
+		movie k_com = new movie();
+		movie e_hor = new movie();
+		movie e_tir = new movie();
+		movie e_dra = new movie();
+		movie e_act = new movie();
+		movie e_com = new movie();
+		movie h_hor = new movie();
+		movie h_tir = new movie();
+		movie h_dra = new movie();
+		movie h_act = new movie();
+		movie h_com = new movie();
+		while(true)
+		{
+			if(age < 18)	
+			{
+				System.out.println("Select the genre : \n 1 Cartoon \n 2.Comedy \n 3.Quit");
+				int req = a.nextInt();
+				if(req > 3) {
+					System.out.println("ivalid input: ");
+					System.exit(0);
+				}
+				switch(req)
+				{
+					case 1 : Cartoon();
+					System.out.println("________________________________________________________________________________________________________________________________");
+						break;
+					case 2 : Com.Comedy();
+					System.out.println("________________________________________________________________________________________________________________________________");
+								break;
+					case 3 : System.out.println("END");
+								System.exit(0);
+				}		
+				//cartoon
+				//comedy
+			}
+			else if(age >= 18 )
+			{
+				while(true)
+				{
+				System.out.println("Select  the langague u want \n1 : kannada \n2 : English \n3 : Hindi \n4 : End ");
+				int lang = a.nextInt();
+				if(lang >= 5) {
+					System.out.println("ivalid input: ");
+					System.exit(0);
+				}
+				switch(lang)
+				{
+					case  1  :System.out.println("Select the genre : \n 1.Horror \n 2.Thriller \n 3.Drama \n 4.Action \n 5.Comedy \n 6.exit");
+					int ch = a.nextInt();
+					if(ch > 7){
+						System.out.println("ivalid input: ");
+						System.exit(0);
+					}
+					
+					switch(ch) 
+					{
+						case 1 : k_hor.k_Horror();
+						
+							break;
+						case 2 : k_tir.k_Thriller();
+							break;
+						case 3 : k_dra.k_drama();
+							break;
+						case 4 : k_act.k_action();
+							break;
+						case 5 : k_com.k_comedy();
+							break;
+						case 6 : System.out.println("END");
+						System.exit(0);
+					}
+					System.out.println("________________________________________________________________________________________________________________________________");
+					break;
+					case 2 :
+						System.out.println("Select the genre : \n 1.Horror \n 2.Thriller \n 3.Drama \n 4.Action \n 5.Comedy \n 6.exit");
+						int chi = a.nextInt();
+						if(chi > 7){
+							System.out.println("ivalid input: ");
+							System.exit(0);
+						}
+						switch(chi) 
+						{
+							case 1 : e_hor.e_Horror();
+								break;
+							case 2 : e_tir.e_Thriller();
+								break;
+							case 3 : e_dra.e_drama();
+								break;
+							case 4 : e_act.e_action();
+								break;
+							case 5 : e_com.e_comedy();
+								break;
+							case 6 : System.out.println("END");
+								System.exit(0);
+						}
+						System.out.println("________________________________________________________________________________________________________________________________");
+						break;
+					case 3 :
+						System.out.println("Select the genre : \n 1.Horror \n 2.Thriller \n 3.Drama \n 4.Action \n 5.Comedy \n 6.exit");
+						int che = a.nextInt();
+						if(che > 7){
+							System.out.println("ivalid input: ");
+							System.exit(0);
+						}
+						switch(che) 
+						{
+							case 1 : h_hor.h_Horror();
+								break;
+							case 2 : h_tir.h_Thriller();
+								break;
+							case 3 : h_dra.h_drama();
+								break;
+							case 4 : h_act.h_action();
+								break;
+							case 5 : h_com.h_comedy();
+								break;
+							case 6 : System.out.println("END");
+								System.exit(0);
+						}
+						System.out.println("________________________________________________________________________________________________________________________________");
+						break;
+					case 4 :
+						System.out.println("END");
+						System.exit(0);
+				}
+				}
+				
+				//horror
+				//suspense
+				//drama
+				//comedy
+				//action
+			}
+		}
+	}
+}
